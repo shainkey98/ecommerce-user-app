35a12cf20c864411337a3238e3bcb78c3b764446
Shainkey Asati <52102782+shainkey98@users.noreply.github.com>
2021-04-29 21:57:55 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,7 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = "https://flipkart-rest-ser.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
 };
+
